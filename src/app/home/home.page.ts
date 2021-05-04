f815329c889b035500c0e74c9c316d663519edaf
unknown <ujjay99@gmail.com>
2021-05-04 21:49:22 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{NewsFeedService} from '../news-feed.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  articles:any
 
-  constructor() {}
+  constructor(private newsService:NewsFeedService) {
+    this.loadNews()
+  }
+  loadNews(){
+    this.newsService.getNews("top-headlines?country=us").subscribe( news=> {
+      this.articles=news['articles'];
+      console.log(this.articles);
+    })
+
+}
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  API_Key:'9efcab9eab2a4d09b34e35d35c340a3b',
+  API_URL:'https://newsapi.org/v2'
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
